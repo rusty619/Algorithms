5a17bfe26da7af24b154b795a77d8ba1b6f98967
rusty619 <rusty6916@gmail.com>
2020-10-16 19:38:56 -0400

    Recursions

---
+++ Recursion/secondRecusion.js
@@ -0,0 +1,44 @@
+// function sumRange(num)
+// {
+//     if(num === 1) return 1;
+//     return num + sumRange(num-1);
+// }
+
+// console.log(sumRange(3));
+
+// function factorial(num)
+// {
+//     let total = 1;
+//     for(let i = num; i > 0;i--)
+//     {
+//         total *= i;
+//     }
+//     return total;
+// }
+
+// console.log(factorial(3));
+
+// function factorial(num)
+// {
+//     if(num === 1)
+//       return 1;
+//     else
+//        return num*factorial(num-1);  
+// }
+
+// console.log(factorial(3));
+// function vowelsAndConsonants(s) {
+//   let vowels = ["a", "e", "i", "o", "u"];
+
+//   for(let v of s) {
+//       if(vowels.includes(v))
+//           console.log(v);
+//   }
+  
+//   for(let v of s) {
+//       if(!vowels.includes(v))
+//           console.log(v);
+//   }
+// }
+
+// vowelsAndConsonants("STRaacsaad");
